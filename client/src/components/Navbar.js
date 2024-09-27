cc4a68f59e150a5b43af2be8c92df721a04e9944
Escape <rajputaditya96530@gmail.com>
2024-09-27 22:52:08 +0530

    Improved Navbar Options

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,7 +11,8 @@ const Navbar = () => {
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#support">Support</a></li>
-        <li><a href="#offers">Offers</a></li>
+        
+        <li><a href="#offers">FAQs</a></li>
       </ul>
     </nav>
   );
