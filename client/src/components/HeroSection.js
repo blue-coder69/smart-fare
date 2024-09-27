c0a852d111064f76f45ab3b2cb9a1c652ad7b0e5
Ayush16110 <newayushgupta2017@gmail.com>
2024-09-27 22:41:30 +0530

    Improved CSS of Hero Section

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -6,16 +6,20 @@ const HeroSection = () => {
   return (
     <section className="hero">
       <div className='Image'>
-      <img src={captainImage} alt="Captain" /> {/* Use the imported image */}
+        <img src={captainImage} alt="Captain" /> Use the imported image
       </div>
       <div className='cards'>
-        <h1>Compare. Choose. Go.</h1>
-        <p>Seamlessly compare cab fares across platforms and book the best deal in seconds</p>
-        <input type='text' placeholder='Enter Your Location' className='location' />
-        <input type='text' placeholder='Enter Destination' className='destination' />
-      </div>
-      <div className='btn'>
-        <button>See The fares</button>
+
+        <div className='left'>
+          <h1>Compare. Choose. Go.</h1>
+          <p>Seamlessly compare cab fares across platforms and book the best deal in seconds</p>
+        </div>
+        <div className='right'>
+          <input type='text' placeholder='Enter Your Location' className='location' />
+          <input type='text' placeholder='Enter Destination' className='destination' />
+          <button className='btn'>See The fares</button>
+        </div>
+        
       </div>
     </section>
   );
