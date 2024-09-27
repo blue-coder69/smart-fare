375ad5f77bbcc09c904a70a3e29d2afc0b01789d
Escape <rajputaditya96530@gmail.com>
2024-09-27 16:14:01 +0530

    Improved Css of WhyChooseUs

--- client/src/components/WhyChooseUs.js
+++ client/src/components/WhyChooseUs.js
@@ -7,16 +7,16 @@ const WhyChooseUs = () => {
       <h2>Why Choose Us?</h2>
       <div className="features">
         <div className="feature">
-          <h3>Reliable Service</h3>
-          <p>Adipiscing turpis sed faucibus magna at sit tincidunt.</p>
+          <h3>Multiple Ride Options</h3>
+          <p>Whether you need a cab, auto, or bike, our app gives you the freedom to choose the best option that fits your budget and needs..</p>
         </div>
         <div className="feature">
-          <h3>Safety First</h3>
-          <p>Adipiscing turpis sed faucibus magna at sit tincidunt.</p>
+          <h3>Transparent Pricing</h3>
+          <p>We believe in providing clear and transparent pricing. No hidden fees or surprises—just accurate fare comparisons across multiple platforms.</p>
         </div>
         <div className="feature">
           <h3>User-Friendly App</h3>
-          <p>Adipiscing turpis sed faucibus magna at sit tincidunt.</p>
+          <p>Our app is designed with ease of use in mind, ensuring that you can compare fares and book your ride in just a few clicks.</p>
         </div>
       </div>
     </section>
