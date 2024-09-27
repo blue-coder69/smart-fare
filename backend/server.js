dad5c7a5726216f6ae4ecdc29a05a982bf8c0345
Escape <rajputaditya96530@gmail.com>
2024-09-28 01:22:13 +0530

    Set up Expres s server and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+
+const ridesRoutes = require('./routes/rides');
+app.use('/api/rides', ridesRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
