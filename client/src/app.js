4c4f232bbc4cab8283eb59d8f6c0c0e38b1fef3f
Ayush16110 <newayushgupta2017@gmail.com>
2024-09-27 15:32:57 +0530

    Fixed some minor bugs

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Compare from './pages/compare';
 import Home from './pages/Home';
 import './styles/global.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Compare />
       <Home/>
     </div>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import AboutUs from '../components/AboutUs';
 import WhyChooseUs from '../components/WhyChooseUs';
-import Testimonials from '../components/Testimonials';
 
 const Home = () => {
   return (
     <div className="home">
+      <Navbar/>
       <HeroSection />
       <AboutUs />
       <WhyChooseUs />
-      <Testimonials />
     </div>
   );
 };
